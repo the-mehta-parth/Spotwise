509deaac9b1d01415a01cda8c9caec980fc581ad
Parth Mehta <mehtabs722.bm@gmail.com>
2025-03-24 21:36:40 +0530

    feat: add json output route to backend

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/upload")
-async def upload_image(image: UploadFile = File(...)):
+# @app.post("/upload")
+# async def upload_image(image: UploadFile = File(...)):
+#     if not image.content_type.startswith("image/"):
+#         raise HTTPException(status_code=400, detail="Invalid file type. Must be an image.")
+
+#     try:
+#         with open(f"input/{image.filename}", "wb") as f:
+#             contents = await image.read()
+#             f.write(contents)
+
+#         yolo_nas_l(f"input/{image.filename}")
+
+#         with open(f"output/{image.filename}", "rb") as image_file:
+#             encoded_string = base64.b64encode(image_file.read()).decode()
+
+#         return {"image": encoded_string}
+
+#         # return FileResponse(f"output/{image.filename}", media_type="image/png")
+
+#     except Exception as e:
+#         print(f"Error uploading image: {e}") # Important for debugging
+#         raise HTTPException(status_code=500, detail=f"Error uploading image: {e}")
+
+@app.post("/upload-json")
+async def upload_image_json(image: UploadFile = File(...)):
     if not image.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Invalid file type. Must be an image.")
 
@@ -26,20 +49,14 @@ async def upload_image(image: UploadFile = File(...)):
             contents = await image.read()
             f.write(contents)
 
-        yolo_nas_l(f"input/{image.filename}")
+        json_response= yolo_nas_l(f"input/{image.filename}")
 
-        with open(f"output/{image.filename}", "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode()
-
-        return {"image": encoded_string}
-
-        # return FileResponse(f"output/{image.filename}", media_type="image/png")
+        return json_response
 
     except Exception as e:
-        print(f"Error uploading image: {e}") # Important for debugging
+        print(f"Error uploading image: {e}")  # Important for debugging
         raise HTTPException(status_code=500, detail=f"Error uploading image: {e}")
-
-
+    
 # Testing Frontend
 @app.get("/")
 async def read_root():
@@ -51,7 +68,7 @@ async def read_root():
     </head>
     <body>
         <h1>Upload Image</h1>
-        <form method="post" action="/upload" enctype="multipart/form-data">
+        <form method="post" action="/upload-json" enctype="multipart/form-data">
             <input type="file" name="image" accept="image/*">
             <input type="submit" value="Upload">
         </form>
